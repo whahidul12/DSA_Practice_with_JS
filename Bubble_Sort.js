96e07839721e1382eff1ac42aab039d82a525f5d
whahidul12 <mdislamwhahid@gmail.com>
2024-12-29 22:05:44 +0600

    first commit

---
+++ Bubble_Sort.js
@@ -0,0 +1,17 @@
+function BubbleSort(myArr) {
+    let swap;
+    do {
+        swap = false;
+        for (let i = 0; i < myArr.length; i++) {
+            if (myArr[i] > myArr[i + 1]) {
+                let temp = myArr[i];
+                myArr[i] = myArr[i + 1];
+                myArr[i + 1] = temp;
+                swap = true;
+            }
+        }
+    } while (swap);
+}
+let myArr = [7, 12, 9, 11, 3, -2];
+BubbleSort(myArr);
+console.log(myArr);
