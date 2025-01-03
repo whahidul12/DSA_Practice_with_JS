4101180a54126fa4ee1f046d57e46692ff257f13
whahidul12 <mdislamwhahid@gmail.com>
2025-01-03 23:50:44 +0600

    Add anoObj class with methods for sleep and drive functionality

--- promo.js
+++ promo.js
@@ -1,5 +1,23 @@
 "use strict";
 
-console.time("time: ");
+class anoObj {
+    constructor(nam, age) {
+        this.name = {
+            firstname: "Whahidul",
+            lastname: "Islam"
+        };
+        this.age = age;
+        this.gender = "male";
+        this.email = "email@m.com";
+        this.sleep = function sleep() {
+            return console.log(`${this.name.firstname} ${this.name.lastname} is sleeping...zzz`);
+        };
+        this.drive = function drive() {
+            return console.log(`${this.name.firstname} ${this.name.lastname} is ${this.age} years old and ${nam}`);
+        };
+    }
+}
 
-console.timeEnd("time: ");
+const jack = new anoObj("pizza", 19);
+console.log(jack.drive());
+
