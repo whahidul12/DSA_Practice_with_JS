3a43722daf6c967503ade19a6be8dfb04d473f5e
whahidul12 <mdislamwhahid@gmail.com>
2025-01-13 18:47:37 +0600

    Refactor Person object structure and add methods for eating, walking, and sleeping

--- promo.js
+++ promo.js
@@ -1,24 +1,29 @@
 "use strict";
 
-const person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 50,
-    eyeColor: "blue"
-};
-function Pperson(first, last, age, eye) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.eyeColor = eye;
+const Person = {
+    name: "whahidul islam",
+    age: 21,
+    sex: "male",
+    eat: () => {
+        console.log("he is eating");
+
+    },
+    walk: () => {
+        console.log("he is walking");
+
+    },
+    sleep: function () {
+        console.log("sleeping...");
+
+    }
+
 }
 
-// Create 2 Person Objects
-const myFather = new Pperson("John", "Doe", 50, "blue");
-const myMother = new Pperson("Sally", "Rally", 48, "green");
+console.log(Person);
 
-// Add a new Property
-Person.nationality = "English";
+Person.age = 22;
+console.log(Person);
+
 
 
 
